25302df700b31d905916066a9960e3db956f9896
tfolly <tfolly@student.42.fr>
2016-01-12 18:16:18 +0100

    fix segfault due a la mauvaise utilisation de strchr

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: tfolly <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/10 11:15:06 by tfolly            #+#    #+#             */
-/*   Updated: 2016/01/11 19:41:09 by tfolly           ###   ########.fr       */
+/*   Updated: 2016/01/12 18:12:04 by tfolly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,18 @@ int			ft_printf(const char *format, ...)
 	va_list args;
 	t_fmt	*begin_list;
 	t_fmt	*list;
-	char	*tmp;
 	int		count;
-	void	*var;
+	t_opt	*opt;
 
 	count = 0;
 	list = fmt_init(list);
 	begin_list = list;
 	va_start(args, format);
+	if (!(opt = (t_opt*)ft_memalloc(sizeof(t_opt))))
+		return (-1);
 	while (*format)
 	{
+		opt_init(opt);
 		while (*format != '%' && *format)
 		{
 			ft_putchar(*format);
@@ -36,6 +38,7 @@ int			ft_printf(const char *format, ...)
 		if (*format == '%')
 		{
 			format++;
+			opt_read(opt, (char**)&format);
 			list = find_list(begin_list, *format);
 			count += (int)list->fct((void*)va_arg(args, void*));
 			format++;
---
+++ opt_read.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   opt_read.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tfolly <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/01/12 16:27:16 by tfolly            #+#    #+#             */
+/*   Updated: 2016/01/12 18:10:55 by tfolly           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+t_opt	*opt_init(t_opt *opt)
+{
+	opt->hash = 0;
+	opt->zero = 0;
+	opt->minus = 0;
+	opt->plus = 0;
+	opt->space = 0;
+	opt->min_width = 0;
+	opt->precision = 0;
+	opt->size = 0;
+	return (opt);
+}
+
+static t_opt	*opt_read_size(t_opt *opt, char **fmt)
+{
+	char	*format;
+
+	format = *fmt;
+	if (*format == 'h' && *(format + 1) == 'h')
+		opt->size = 1;
+	else if (*format == 'h')
+		opt->size = 2;
+	else if (*format == 'l')
+		opt->size = 4;
+	else if (*format == 'l' && *(format + 1) == 'l')
+		opt->size = 8;
+	else if (*format == 'j') //a voir
+		opt->size = 1;
+	else if (*format == 'z') // a voir
+		opt->size = 1;
+	return (opt);
+	
+}
+
+t_opt	*opt_read(t_opt *opt, char **fmt)
+{
+	char	*format;
+	char	*opt_str;
+
+	opt_str = ft_strdup("+-0# ");
+	format = *fmt;
+	while (ft_strchr(opt_str, *format))
+	{
+		if (*format == '#')
+			opt->hash = 1;
+		else if (*format == '0')
+			opt->zero = 1;
+		else if (*format == '+')
+			opt->plus = 1;
+		else if (*format == '-')
+			opt->minus = 1;
+		else if (*format == ' ')
+			opt->space = 1;
+		format++;
+	}
+	while (ft_isdigit(*format))
+	{
+		opt->min_width *= 10;
+		opt->min_width += *format;
+		format++;
+	}
+	if (*format == '.')
+	{
+		format++;
+		while (ft_isdigit(*format))
+		{
+			opt->precision *= 10;
+			opt->precision += *format;
+			format++;
+		}
+	}
+	opt = opt_read_size(opt, fmt);
+	return (opt);
+}
